d22065c5ccf816484d2d67309755046936ed1f90
Bernardo Antunes Heckler <bernaheckler1@gmail.com>
2024-06-25 21:06:22 -0300

    Criação do Projeto para Adaptação, a pasta assets foi renomeada para Recursos, e as bibliotecas dentro do código também.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # pip install cx_freeze
 import cx_Freeze
 executaveis = [ 
-               cx_Freeze.Executable(script="main.py", icon="assets/icone.ico") ]
+               cx_Freeze.Executable(script="main.py", icon="Recursos/icone.ico") ]
 cx_Freeze.setup(
     name = "Iron Man",
     options={
         "build_exe":{
             "packages":["pygame"],
-            "include_files":["assets"]
+            "include_files":["Recursos"]
         }
     }, executables = executaveis
 )
